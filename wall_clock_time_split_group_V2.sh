36c1d0a4bda5533c32e00af182edd0dc99a031bf
Mike Mikailov <mmikailov@gmail.com>
2020-03-21 10:42:13 -0400

    Add files via upload

---
+++ wall_clock_time_split_group_V2.sh
@@ -0,0 +1,33 @@
+# timing for splitting and grouping 
+
+D="$2"/sysout
+
+PREFIX=sg_testing
+
+case "$1" in
+   "normal") PREFIX=split_grp 
+   ;;
+   "test") PREFIX=sg_testing 
+   ;;
+   "tumor") PREFIX=split_grp 
+   ;;
+esac
+
+DIR="$1"_timing
+SUFFIX=`date '+%Y%m%d%H%M%S'`
+mv $DIR "$DIR"_"$SUFFIX" 2>/dev/null
+mkdir -p $DIR
+
+FN_PR=$DIR/"$1"_split_timing
+
+echo "PREFIX=$PREFIX"
+echo "D=$D"
+
+find $D -name "$PREFIX"* | xargs grep "real" > "$FN_PR".txt
+
+awk -F ' ' '{print $2}' "$FN_PR".txt > "$FN_PR"_nums.txt 
+sed 's/m/ /g; s/s//g' "$FN_PR"_nums.txt > "$FN_PR"_nums2.txt # remove all "m" "s"
+sort -k1 -k2 -n -r "$FN_PR"_nums2.txt > "$FN_PR"_nums2_sorted.txt
+awk '{ printf ("%.18f\n", $1 * 60 + $2) }' "$FN_PR"_nums2_sorted.txt > "$FN_PR"_final_sorted.txt
+
+
