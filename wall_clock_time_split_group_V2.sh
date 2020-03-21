dd60cc98a1fad46d225df2546a54b95f32cf7c9c
Mike Mikailov <mmikailov@gmail.com>
2020-03-21 11:10:47 -0400

    Add files via upload

--- wall_clock_time_split_group_V2.sh
+++ wall_clock_time_split_group_V2.sh
@@ -13,21 +13,17 @@ case "$1" in
    ;;
 esac
 
-DIR="$1"_timing
+DIR=wall_clock_time_details_"$1"
 SUFFIX=`date '+%Y%m%d%H%M%S'`
 mv $DIR "$DIR"_"$SUFFIX" 2>/dev/null
 mkdir -p $DIR
 
-FN_PR=$DIR/"$1"_split_timing
-
-echo "PREFIX=$PREFIX"
-echo "D=$D"
+FN_PR=$DIR/time
 
 find $D -name "$PREFIX"* | xargs grep "real" > "$FN_PR".txt
 
 awk -F ' ' '{print $2}' "$FN_PR".txt > "$FN_PR"_nums.txt 
 sed 's/m/ /g; s/s//g' "$FN_PR"_nums.txt > "$FN_PR"_nums2.txt # remove all "m" "s"
 sort -k1 -k2 -n -r "$FN_PR"_nums2.txt > "$FN_PR"_nums2_sorted.txt
-awk '{ printf ("%.18f\n", $1 * 60 + $2) }' "$FN_PR"_nums2_sorted.txt > "$FN_PR"_final_sorted.txt
-
+awk '{ printf ("%.18f\n", $1 * 60 + $2) }' "$FN_PR"_nums2_sorted.txt > wall_clock_final_"$SUFFIX".txt
 
